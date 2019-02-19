2c5017783276f464b184d7d7eee7d782d4b38efb
jocmom <jombi@web.de>
2019-02-19 13:42:09 +0100

    ADD: Sqlite support

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API
 {
